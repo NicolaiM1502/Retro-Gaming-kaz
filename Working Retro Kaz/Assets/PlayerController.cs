3de35a27473c122a096e5488e32c1b878352136d
NicolaiM1502 <115453812+NicolaiM1502@users.noreply.github.com>
2023-04-24 12:51:45 +0200

    animation

--- Working Retro Kaz/Assets/PlayerController.cs
+++ Working Retro Kaz/Assets/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 
 //håndtere movement fra player
-public class playermove : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
 
@@ -14,6 +14,7 @@ public class playermove : MonoBehaviour
     public ContactFilter2D movementfilter;
 
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     Animator animator;
     List<RaycastHit2D> CastCollisions = new List<RaycastHit2D>();
@@ -23,6 +24,7 @@ public class playermove : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate() { 
@@ -32,20 +34,26 @@ public class playermove : MonoBehaviour
 
             if(!success) {
                 success = TryMove(new Vector2(movementInput.x, 0));
-            
+            }
                 if(!success) {
                     success = TryMove(new Vector2(0, movementInput.y));
                 }
 
             }
             
-            
-        } 
-            
-        
+            animator.SetBool("isMoving", success);
+        } else {
+            animator.SetBool("isMoving", false);
+            }
+        if(movementInput.x < 0) {
+            spriteRenderer.flipX = true;
+        } else if (movementInput.x > 0){
+            spriteRenderer.flipX = false;
+        }
     }
 
 private bool TryMove(Vector2 direction){ 
+    if(direction != Vector2.zero) {
         int count = rb.Cast(
             direction,
             movementfilter,
@@ -56,11 +64,13 @@ private bool TryMove(Vector2 direction){
             if (count == 0){
                 rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
                 return true;
-            } else {
+        } else {
                 return false;
-            }
-        }
-
+        } 
+    } else {
+        return false;
+}
+    
 
     void OnMove(InputValue movementValue) => movementInput = movementValue.Get<Vector2>();
 
